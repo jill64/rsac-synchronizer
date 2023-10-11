9f92bbf80e5f7ae5344c1f24cd30328ec7d8b269
jill64 <intents.turrets0h@icloud.com>
2023-10-11 17:49:22 +0900

    fix: fix: src

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import memoize from 'lodash/memoize.js'
 import { octoflare } from 'octoflare'
 
 export default octoflare(async ({ app, installation, payload }) => {
@@ -15,26 +16,62 @@ export default octoflare(async ({ app, installation, payload }) => {
 
   const { repository, commits } = payload
 
-  const isTriggered = commits.some((commit) =>
-    commit.modified.includes('rsac.yml')
-  )
+  const owner = repository.owner.login
+  const repo = repository.name
+  const ref = payload.ref.replace('refs/heads/', '')
 
-  if (!isTriggered) {
-    return new Response("Not found 'rsac.yml'", {
+  if (ref !== repository.default_branch) {
+    return new Response('Base is not default branch', {
       status: 200
     })
   }
 
-  await app.octokit.rest.actions.createWorkflowDispatch({
+  const isOwner = memoize(async (name: string) => {
+    if (name === owner) {
+      return true
+    }
+
+    if (repository.owner.type !== 'Organization') {
+      return false
+    }
+
+    const {
+      data: { role }
+    } = await installation.kit.rest.orgs.getMembershipForUser({
+      org: owner,
+      username: name
+    })
+
+    return role === 'admin'
+  })
+
+  const isTriggered = commits
+    .filter((commit) => commit.modified.includes('rsac.yml'))
+    .every((commit) => isOwner(commit.author.name))
+
+  if (!isTriggered) {
+    return new Response('Not found valid commit', {
+      status: 200
+    })
+  }
+
+  const octokit = await app.octokit.rest.apps
+    .getRepoInstallation({
+      owner: 'jill64',
+      repo: 'rsac-synchronizer'
+    })
+    .then(({ data: { id } }) => app.getInstallationOctokit(id))
+
+  await octokit.rest.actions.createWorkflowDispatch({
     owner: 'jill64',
     repo: 'rsac-synchronizer',
     workflow_id: 'synchronize.yml',
     ref: 'main',
     inputs: {
       token: installation.token,
-      owner: repository.owner.login,
-      repo: repository.name,
-      ref: payload.ref.replace('refs/heads/', '')
+      owner,
+      repo,
+      ref
     }
   })
 
