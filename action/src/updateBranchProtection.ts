05f7c96f9ead746efc5f99abcc3f29cd33619174
jill64 <intents.turrets0h@icloud.com>
2023-10-11 18:35:29 +0900

    add action source

---
+++ action/src/updateBranchProtection.ts
@@ -0,0 +1,33 @@
+import github from '@actions/github'
+import { isBranchProtection } from './isBranchProtection.js'
+
+export const updateBranchProtection = async ({
+  protections,
+  octokit,
+  owner,
+  repo
+}: {
+  protections: Record<string, unknown>
+  octokit: ReturnType<typeof github.getOctokit>
+  owner: string
+  repo: string
+}) => {
+  const branches = Object.keys(protections)
+
+  const result = branches.map(async (branch) => {
+    const conf = protections[branch]
+
+    if (!isBranchProtection(conf)) {
+      return
+    }
+
+    await octokit.rest.repos.updateBranchProtection({
+      owner,
+      repo,
+      branch,
+      ...conf
+    })
+  })
+
+  await Promise.all(result)
+}
