688e6a199b153b4fe4ccaf5183063a7e49e91a6a
jill64 <intents.turrets0h@icloud.com>
2023-10-11 16:30:02 +0900

    add src

---
+++ src/index.ts
@@ -0,0 +1,44 @@
+import { octoflare } from 'octoflare'
+
+export default octoflare(async ({ app, installation, payload }) => {
+  if (!('commits' in payload)) {
+    return new Response('No Push Event', {
+      status: 200
+    })
+  }
+
+  if (!installation) {
+    return new Response('No Installation', {
+      status: 200
+    })
+  }
+
+  const { repository, commits } = payload
+
+  const isTriggered = commits.some((commit) =>
+    commit.modified.includes('rsac.yml')
+  )
+
+  if (!isTriggered) {
+    return new Response("Not found 'rsac.yml'", {
+      status: 200
+    })
+  }
+
+  await app.octokit.rest.actions.createWorkflowDispatch({
+    owner: 'jill64',
+    repo: 'rsac-synchronizer',
+    workflow_id: 'rsac-synchronizer.yml',
+    ref: 'main',
+    inputs: {
+      token: installation.token,
+      owner: repository.owner.login,
+      repo: repository.name,
+      ref: payload.ref.replace('refs/heads/', '')
+    }
+  })
+
+  return new Response(null, {
+    status: 202
+  })
+})
