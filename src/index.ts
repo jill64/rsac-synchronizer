24b6ecb7495b98736cd2aed4978f54b5cb30b6d2
jill64 <intents.turrets0h@icloud.com>
2023-11-16 13:16:02 +0900

    Update repository bootstrap process

--- src/index.ts
+++ src/index.ts
@@ -7,64 +7,55 @@ export default octoflare(async ({ installation, payload }) => {
     })
   }
 
-  if (
-    !(
-      'action' in payload &&
-      'repository' in payload &&
-      payload.action === 'created' &&
-      payload.repository
-    )
-  ) {
+  if (!('repositories_added' in payload && payload.repositories_added)) {
     return new Response('No Trigger Event', {
       status: 200
     })
   }
 
-  const { repository } = payload
-
-  const owner = repository.owner.login
-  const repo = repository.name
-
-  const process = [
-    installation.kit.rest.repos.update({
-      owner,
-      repo,
-      has_projects: false,
-      has_wiki: false,
-      allow_squash_merge: false,
-      allow_rebase_merge: false,
-      allow_auto_merge: true,
-      delete_branch_on_merge: true,
-      allow_update_branch: true
-    }),
-    installation.kit.rest.repos.updateBranchProtection({
-      owner,
-      repo,
-      branch: repository.default_branch,
-      required_status_checks: {
-        strict: true,
-        contexts: ['Wraith CI', 'Wraith CI / PR']
-      },
-      enforce_admins: true,
-      required_pull_request_reviews: {
-        required_approving_review_count: 0
-      },
-      restrictions: null,
-      allow_deletions: false,
-      lock_branch: false,
-      allow_force_pushes: false
-    }),
-    installation.kit.rest.actions.setGithubActionsDefaultWorkflowPermissionsRepository(
-      {
-        owner,
-        repo,
-        default_workflow_permissions: 'read',
-        can_approve_pull_request_reviews: false
-      }
-    )
-  ]
-
-  await Promise.all(process)
+  await Promise.allSettled(
+    payload.repositories_added.map(async ({ full_name }) => {
+      const [owner, repo] = full_name.split('/')
+      return await Promise.allSettled([
+        installation.kit.rest.repos.update({
+          owner,
+          repo,
+          has_projects: false,
+          has_wiki: false,
+          allow_squash_merge: false,
+          allow_rebase_merge: false,
+          allow_auto_merge: true,
+          delete_branch_on_merge: true,
+          allow_update_branch: true
+        }),
+        installation.kit.rest.repos.updateBranchProtection({
+          owner,
+          repo,
+          branch: 'main',
+          required_status_checks: {
+            strict: true,
+            contexts: ['Wraith CI', 'Wraith CI / PR']
+          },
+          enforce_admins: true,
+          required_pull_request_reviews: {
+            required_approving_review_count: 0
+          },
+          restrictions: null,
+          allow_deletions: false,
+          lock_branch: false,
+          allow_force_pushes: false
+        }),
+        installation.kit.rest.actions.setGithubActionsDefaultWorkflowPermissionsRepository(
+          {
+            owner,
+            repo,
+            default_workflow_permissions: 'read',
+            can_approve_pull_request_reviews: false
+          }
+        )
+      ])
+    })
+  )
 
   return new Response('Bootstrap Complete', {
     status: 200
